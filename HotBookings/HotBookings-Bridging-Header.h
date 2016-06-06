54f5f6f3b8f0c2087c8d4095a69939d5d826b4c8
Chee Yi Ong <chong@expedia.com>
2016-06-06 11:06:03 -0500

    Added CompactConstraint and a bridging header

---
+++ HotBookings/HotBookings-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  HotBookings-Bridging-Header.h
+//  HotBookings
+//
+//  Created by Chee Yi Ong on 6/6/16.
+//  Copyright © 2016 Expedia Inc. All rights reserved.
+//
+
+#import "CompactConstraint.h" 
